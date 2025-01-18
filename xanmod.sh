84ee2039a37cced7bcb9e541bf8e59cf63b9bce9
coldrook <8768561+coldrook@users.noreply.github.com>
2025-01-18 17:08:18 +0800

    Update xanmod.sh

--- xanmod.sh
+++ xanmod.sh
@@ -36,8 +36,7 @@ echo "CPU ABI 检测结果："
 echo "$cpu_abi_output"
 echo "提取的 ABI Level: $cpu_abi_level"
 
-# 在这里暂停执行
-read -p "按 Enter 继续安装内核..."
+# read -p "按 Enter 继续安装内核..."
 
 # 6. 根据 CPU 支持安装相应的 XanMod 内核
 echo "6. 根据 CPU 支持安装 XanMod 内核..."
