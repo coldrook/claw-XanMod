a95c03dca33b16cf390a8d81549601eb6aae9182
coldrook <8768561+coldrook@users.noreply.github.com>
2025-01-18 16:43:32 +0800

    Update xanmod.sh

--- xanmod.sh
+++ xanmod.sh
@@ -25,17 +25,17 @@ sudo apt update
 echo "5. 检查 CPU 支持的 x86-64 ABI..."
 wget https://raw.githubusercontent.com/yumaoss/xanmod_tools/refs/heads/main/check_x86-64_psabi.sh
 chmod +x check_x86-64_psabi.sh
-./check_x86-64_psabi.sh
+OUTPUT=$(./check_x86-64_psabi.sh)
 
 # 6. 根据 CPU 支持安装相应的 XanMod 内核
 echo "6. 根据 CPU 支持安装 XanMod 内核..."
-if grep -q "CPU supports x86-64-v4" <<< "$(/bin/bash check_x86-64_psabi.sh)"; then
+if grep -q "CPU supports x86-64-v4" <<< "$OUTPUT"; then
   echo "CPU 支持 x86-64-v4，安装 linux-xanmod-x64v4"
   sudo apt install -y linux-xanmod-x64v4
-elif grep -q "CPU supports x86-64-v3" <<< "$(/bin/bash check_x86-64_psabi.sh)"; then
+elif grep -q "CPU supports x86-64-v3" <<< "$OUTPUT"; then
   echo "CPU 支持 x86-64-v3，安装 linux-xanmod-x64v3"
   sudo apt install -y linux-xanmod-x64v3
-elif grep -q "CPU supports x86-64-v2" <<< "$(/bin/bash check_x86-64_psabi.sh)"; then
+elif grep -q "CPU supports x86-64-v2" <<< "$OUTPUT"; then
   echo "CPU 支持 x86-64-v2，安装 linux-xanmod-x64v2"
   sudo apt install -y linux-xanmod-x64v2
 else
@@ -43,7 +43,6 @@ else
   echo "请检查 CPU 支持并手动选择合适的内核。"
   exit 1
 fi
-
 
 # 7. 清理临时文件
 echo "7. 清理临时文件..."
